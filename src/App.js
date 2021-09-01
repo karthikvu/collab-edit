7bbef482444bd2693ea94c8ed3620edb50ee80e2
Karthik VU <karthik.vu@vistaprint.com>
2021-09-01 12:16:40 +0530

    Cursor position sharing enabled

--- src/App.js
+++ src/App.js
@@ -5,44 +5,64 @@ import logo from './logo.svg';
 import './App.css';
 import Cursor from './components/Cursor';
 import useMousePosition from './hooks/useMousePosition';
-
+import { throttle } from './utils'
 
 let socket;
 
+const throttledEmit = throttle((position) => {
+  if (!socket) return;
+  socket.emit("cursorPosition", JSON.stringify(position));
+}, 100)
+
+const byId = arr => {
+  return arr.reduce((acc, curr) => {
+    acc[curr.id] = curr;
+    return acc;
+  }, {});
+}
 
 function App() {
+  const [myId, setMyId] = useState('')
   const [cursor, setCursor] = useState("")
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState({})
   const position = useMousePosition()
 
   useEffect(() => {
     socket = io("http://localhost:3001", {});
-    socket.on('test', d => {
-      console.log(d)
+    socket.on('_connect', function (d) {
+      setMyId(d)
+    });
+    
+    socket.on('cursorPosition', d => {
+      setUsers({
+        ...users,
+        [d.id]:d
+      })
     })
+
     socket.on('users', users => {
-      console.log(users)
-      setUsers(users)
+      setUsers(byId(users))
     })
 
     fetch('https://picsum.photos/300/300?random=1').then(resp => {
       setCursor(resp.url)
       socket.emit("image", resp.url);
     }).catch(console.log)
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useEffect(() => {
-    socket.emit("cursorPosition", JSON.stringify(position));
+    throttledEmit(position)
   }, [position])
+  console.log(`users: `,users)
   
   return (
     <div className="App">
       {/* My cursor */}
-      <Cursor img={cursor} {...position} />
+      <Cursor img={cursor} name={ myId} {...position} />
       {/* Others Cursors */}
-      {users.map((user, i) => {
-        return <Cursor key={ user.id} img={user.url} x={i * 100} y={i * 100} />
+      {Object.values(users).map((user, i) => {
+        return <Cursor key={user.id} name={user.id } {...user} img={user.url} />
       })
       }
     </div>
--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -1,14 +1,20 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
+import { motion } from "framer-motion"
+
 import "./cursor.css"
 
 export default function Cursor(props) {
+    console.log(props)
     const { x, y, img } = props
     return (
-        <img className="cursor" width="60px" height="60px" src={img} style={{
+        <motion.div layout={props.name}  style={{
             position: 'absolute',
             top: y,
             left: x
-        }} />
+        }} >
+            <img className="cursor" width="60px" height="60px" src={img}/>
+            <span>{props.name}</span>
+        </motion.div>
     )
 }
